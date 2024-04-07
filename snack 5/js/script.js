2e3be1d8f419165c94ac9d29552e6ba4b7739780
Om12ek99 <omar.elkamili@gmail.com>
2024-04-08 01:55:28 +0200

    add:snack 5 html e js

---
+++ snack 5/js/script.js
@@ -0,0 +1,11 @@
+const oddNumb = [];       //array vyoto
+
+// ciclo for di controllo se il numero è dispari
+for (i = 0; i < 6; i++) {
+    userNumber = parseInt(prompt("inserisci un numero"))
+    if (userNumber % 2 !== 0) {
+        oddElments.push(userNumber);        //comado di push che inserisce il valore corretto nell'array
+    }
+}
+// comando console
+alert(`i numeri dispari inseriti sono ${oddElments}`); 
