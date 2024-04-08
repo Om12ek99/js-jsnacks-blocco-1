41d1523588a40c4cac9d366f36781ac24016b632
Om12ek99 <omar.elkamili@gmail.com>
2024-04-08 02:11:22 +0200

    add:Html e Js for snack 6

---
+++ snack 6/js/script.js
@@ -0,0 +1,8 @@
+const userNumb = parseInt(prompt("inserisci un numero"));  //prompt comando inserimento numero
+
+// comadno for
+for (i = 0; i <= userNumb; i++) {
+    const cube = i * i * i;
+    console.log(i, cube);
+    
+}
